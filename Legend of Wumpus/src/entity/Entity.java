8d266242258a52e487d73f1ef257746540c0da4a
dylan <dylan@Dylan-Ubuntu>
2016-04-26 17:27:05 -0400

    Started to flesh out Entity

--- Legend of Wumpus/src/entity/Entity.java
+++ Legend of Wumpus/src/entity/Entity.java
@@ -1,12 +1,28 @@
 package entity;
 
+import java.awt.Image;
+
+/**This is the abstract Entity class.  It is
+ * the superclass of all dynamic world objects.
+ * */
 public abstract class Entity {
+	//Variables are protected so they can be accessed by subclasses
 	protected int x;
 	protected int y;
-	protected int spriteXSize;
+	protected Image sprite; //This is what is being drawn, not 
+							//a container for all sprite states.
+	protected int spriteXSize;//Again, only for the current sprite
 	protected int spriteYSize;
 	protected double health;
 	
+	//These are things that all entities should know how to do
 	public abstract void collide(Entity e);
 	public abstract void damage(int amount);
+	public abstract void damage(int amount, Entity damageSource);
+	public abstract void die();
+	public abstract void die(Entity damageSource);
+	
+	public boolean isAlive() {
+		return health > 0;
+	}
 }
---
+++ Legend of Wumpus/src/entity/EntityMinion.java
@@ -0,0 +1,6 @@
+package entity;
+
+/**This class is the superclass for all hostile living things.*/
+public abstract class EntityMinion extends Entity {
+	
+}
