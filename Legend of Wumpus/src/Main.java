a790c2d8294ea7223464b19929de90909a2e1d3a
Dylan <dylanjones2011@gmail.com>
2016-04-30 12:51:04 -0400

    Worked on Audio code

--- Legend of Wumpus/src/Main.java
+++ Legend of Wumpus/src/Main.java
@@ -5,14 +5,16 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.sound.sampled.*;
 import javax.swing.*;
 
 import entity.*;
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
-import sun.audio.ContinuousAudioDataStream;
+
 
 public class Main {
+	private static Clip clip;
+	private static AudioInputStream audioIn;
+	
 	public static void main(String[] args) throws InterruptedException {
 		JFrame frame = new JFrame();
 		JPanel panel = new WumpusPanel();
@@ -20,18 +22,28 @@ public class Main {
 		frame.add(panel);
 		frame.setSize(600, 600);
 		frame.setVisible(true);
-		/*TEMPORARY, DELETE SOON*/
+		/* TEMPORARY, DELETE SOON */
 		new EntityGremlin(100, 100);
 		new EntityGremlin(100, 500);
 		new EntityGremlin(500, 100);
 		new EntityGremlin(500, 500);
 		for (;;) {
-			playSound("assets/SplashScreenTheme.wav");
-			Thread.sleep((long) ((1000 * 60) * 3.51));
+			try {
+				playSound("assets/SplashScreenTheme.wav");
+				Thread.sleep((long) ((1000 * 60) * 3.51));
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+				e.printStackTrace();
+			}
 		}
+
 	}
-	private static void playSound(String file) {
-		AudioPlayer MGP = AudioPlayer.player;
+
+	private static void playSound(String file) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+		audioIn = AudioSystem.getAudioInputStream(Main.class.getResource(file));
+		clip = AudioSystem.getClip();
+		clip.open(audioIn);
+		clip.start();
+		/*AudioPlayer MGP = AudioPlayer.player;
 		AudioStream BGM;
 
 		ContinuousAudioDataStream loop = null;
@@ -40,12 +52,12 @@ public class Main {
 			InputStream test = new FileInputStream(file);
 			BGM = new AudioStream(test);
 			AudioPlayer.player.start(BGM);
-
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		} catch (IOException error) {
-			error.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		MGP.start(loop);
+		// Why are you starting it on a null object
+		// MGP.start(loop);*/
 	}
 }
