ff2b27749d540136f721039c18ddf9dd59048dad
Dylan <dylanjones2011@gmail.com>
2016-05-16 21:39:03 -0400

    Working on Spider AI

--- Legend of Wumpus/src/entity/EntitySpider.java
+++ Legend of Wumpus/src/entity/EntitySpider.java
@@ -7,10 +7,20 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import display.World;
+
 public class EntitySpider extends EntityMinion {
+	private enum Direction {
+		NORTHWEST, SOUTHWEST, SOUTHEAST, NORTHEAST;
+	}
 	private static Image standingImage;
 	private static Image jumpingImage;
-	
+
+	private boolean jumping = false;
+	private long lastJumpTime = 0;
+	private int jumpXDest = 0;
+	private int jumpYDest = 0;
+
 	static {
 		try {
 			standingImage = ImageIO.read(new File("assets/spider/standing.png"));
@@ -30,31 +40,46 @@ public class EntitySpider extends EntityMinion {
 	@Override
 	public void attack(Entity target) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void tick() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void collide(Entity e) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void damage(int amount, Entity damageSource) {
-		// TODO Auto-generated method stub
-
+		World.deregisterEntity(this);
 	}
 
 	@Override
 	public void draw(Graphics g) {
-		// TODO Auto-generated method stub
-
+		if (jumping) {
+			g.drawImage(jumpingImage, x + spriteWidth / 2, y + spriteHeight / 2, null);
+		} else {
+			g.drawImage(standingImage, x + spriteWidth / 2, y + spriteHeight / 2, null);
+		}
 	}
 
+	private Direction whichWayToJump() {
+		int playerX = World.getThePlayer().getX();
+		int playerY = World.getThePlayer().getY();
+		if(playerX > x) {
+			if(playerY > y)
+				return Direction.SOUTHEAST;
+			else
+				return Direction.NORTHEAST;
+		} else {
+			if(playerY > y)
+				return Direction.SOUTHWEST;
+			else
+				return Direction.NORTHWEST;
+		}
+	}
 }
