7575891f23fe27520e9b7fb0f5e6f8ddea45d99d
Dylan <dylanjones2011@gmail.com>
2016-05-26 17:46:06 -0400

    Started on WorldTile enum

---
+++ Legend of Wumpus/src/tiles/WorldTile.java
@@ -0,0 +1,12 @@
+package tiles;
+
+public enum WorldTile {
+	//Misc objects
+	stairs1, square_shrub, stairs2, round_shrub, statue,
+	//Tree tiles
+	trees_nw, trees_sw, trees_ne, trees_se, trees_north, trees_south,
+	//Lake tiles
+	lake_ne, lake_se, lake_nw, lake_sw, lake_north, lake_south, lake_east, lake_west,
+	shore_nw, shore_ne, shore_sw, shore_se
+	//
+}
