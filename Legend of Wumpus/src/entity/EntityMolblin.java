770eb1bdfeb0f63bcf45c47d25a30b80b3eced65
Dylan <dylanjones2011@gmail.com>
2016-06-02 22:31:33 -0400

    Started on EntityMolblin

---
+++ Legend of Wumpus/src/entity/EntityMolblin.java
@@ -0,0 +1,46 @@
+package entity;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class EntityMolblin extends EntityMinion {
+	private static Image northImage;
+	private static Image southImage;
+	private static Image eastImage;
+	private static Image westImage;
+
+	static {
+		// Init images
+	}
+
+	@Override
+	public void attack(Entity target) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void collide(Entity e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void damage(int amount, Entity damageSource) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
