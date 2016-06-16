ae91f052c5c456a22ac734013c474384c139fb4e
Dylan <dylanjones2011@gmail.com>
2016-06-16 18:50:36 -0400

    Changed controls

--- Legend of Wumpus/src/display/Tick.java
+++ Legend of Wumpus/src/display/Tick.java
@@ -32,7 +32,7 @@ public class Tick implements ActionListener {
 	private void movePlayer() {
 		if (kb.isKeyPressed('w'))
 			World.getThePlayer().move(0.15);
-		if (kb.isKeyPressed('e'))
+		if (kb.isKeyPressed('a'))
 			World.getThePlayer().turnLeft();
 		if (kb.isKeyPressed('d'))
 			World.getThePlayer().turnRight();
